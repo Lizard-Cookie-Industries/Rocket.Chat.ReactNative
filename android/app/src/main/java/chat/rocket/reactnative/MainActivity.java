0da2c67839abaf79c47d69ffa39be28c7c78a684
timdc <tim+gh@timdc.com>
2020-07-25 20:58:25 -0400

    fix name consistency

--- android/app/src/main/java/chat/rocket/reactnative/MainActivity.java
+++ android/app/src/main/java/chat/rocket/reactnative/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends ReactFragmentActivity {
     */
     @Override
     protected String getMainComponentName() {
-        return "RocketChatRN";
+        return "SecureUtilityChat";
     }
 
     @Override
